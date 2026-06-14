805db211a1dd9d0975cee1c9d9f08067482580ac
Ruan Pablo <ruanpablomota23@gmail.com>
2026-06-14 18:17:32 -0300

    Criando classe CidadeClima

--- src/CidadeClima.java
+++ src/CidadeClima.java
@@ -1,2 +1,35 @@
-public class CidadeClima {
-}
+    public class CidadeClima {
+    String cidade;
+    int temperatura;
+    String condicao;
+
+        public CidadeClima(String cidade, int temperatura, String condicao) {
+            this.cidade = cidade;
+            this.temperatura = temperatura;
+            this.condicao = condicao;
+        }
+
+        public String getCidade() {
+            return cidade;
+        }
+
+        public void setCidade(String cidade) {
+            this.cidade = cidade;
+        }
+
+        public int getTemperatura() {
+            return temperatura;
+        }
+
+        public void setTemperatura(int temperatura) {
+            this.temperatura = temperatura;
+        }
+
+        public String getCondicao() {
+            return condicao;
+        }
+
+        public void setCondicao(String condicao) {
+            this.condicao = condicao;
+        }
+    }
